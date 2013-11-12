fab9a238a40fefed0ede393f1183c61b5a3a85e0
synthresin <synthresin@bagseong-wan-ui-MacBook-Pro.local>
2013-11-12 12:36:49 +0900

    시작

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AuthExercise::Application.config.secret_key_base = '0badb21075826e1431e59ffcf3e46066f0c402acfdd7561b6922d56b9b1d92237abea9e1e5aa52664268018c061c88c712654b61d7378c353e25660c113ef0ed'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AuthExercise::Application.config.session_store :cookie_store, key: '_auth_exercise_session'
